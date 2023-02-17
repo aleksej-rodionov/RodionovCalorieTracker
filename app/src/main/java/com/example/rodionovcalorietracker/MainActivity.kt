f312dd22f614d638b549a7fa33ebefa2c994b42d
Aleksej <67003544+aleksej-rodionov@users.noreply.github.com>
2023-02-17 18:53:46 +0600

    welcome screen implemented

--- app/src/main/java/com/example/rodionovcalorietracker/MainActivity.kt
+++ app/src/main/java/com/example/rodionovcalorietracker/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.rodionovcalorietracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.onboarding_presentation.welcome.WelcomeScreen
 import com.example.rodionovcalorietracker.ui.theme.RodionovCalorieTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //todo
-
+        setContent {
+            RodionovCalorieTrackerTheme {
+                WelcomeScreen()
+            }
+        }
     }
 }
